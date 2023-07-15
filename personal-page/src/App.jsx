d2ac112d14b248cb3c0003a1ced58f62f5b42c02
choilin310 <choiinhwan0@gmail.com>
2023-07-15 00:49:44 -0400

    css foundation

--- personal-page/src/App.jsx
+++ personal-page/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, Link } from "react-router-dom"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      Hello
-    </>
+    <div>
+      
+    </div>
   )
 }
 
