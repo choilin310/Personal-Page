2066d11f8292655b4503b3be95c1d1a0f471815a
choilin310 <choiinhwan0@gmail.com>
2023-07-17 09:37:45 -0400

    starting on home

---
+++ personal-page/src/components/home/Social.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a
+        href="https://www.instagram.com/dls_ghks1/?hl=en"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-instagram"></i>
+      </a>
+
+      <a
+        href="https://www.linkedin.com/in/inhwanchoi/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-linkedin-alt"></i>
+      </a>
+
+      <a
+        href="https://github.com/choilin310"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i class="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Social;
