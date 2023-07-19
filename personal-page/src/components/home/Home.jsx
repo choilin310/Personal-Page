ddadb234f2aaccf332320505a2ec233dcb0224af
choilin310 <choiinhwan0@gmail.com>
2023-07-18 20:58:10 -0400

    finished home component

--- personal-page/src/components/home/Home.jsx
+++ personal-page/src/components/home/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import "./home.css";
 import Social from "./Social";
-  {
+import Data from "./Data";
+import ScrollDown from "./ScrollDown";
+
+const Home = () => {
   return (
     <section className="home section" id="home">
       <div className="home__container container grid">
@@ -12,6 +15,8 @@ import Social from "./Social";
 
           <Data />
         </div>
+
+        <ScrollDown />
       </div>
     </section>
   );
